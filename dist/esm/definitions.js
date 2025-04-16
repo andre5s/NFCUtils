20db90f735d2f62d8b6f106b95434bedefaf875a
Andrea Sandrin <sandrin.andrea99@gmail.com>
2025-04-17 00:12:04 +0200

    update .gitignore

--- dist/esm/definitions.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview NFCUtils definitions - NFC plugin for Capacitor
- * @license MIT
- *
- * @version 1.0
- * @author Andrea Sandrin
- */
-export {};
-//# sourceMappingURL=definitions.js.map
